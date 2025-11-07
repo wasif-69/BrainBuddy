0208c45009b4017c26ad02d1bd9e1908f4e6f88a
wasif-69 <wasifkhalil69@gmail.com>
2025-11-07 04:00:29 -0800

    adding APId

--- app/src/inputs/Input.jsx
+++ app/src/inputs/Input.jsx
@@ -34,7 +34,7 @@ export default function Input() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://127.0.0.1:8000/myapp/add/", {
+      const response = await fetch("https://brainserver-slq7.onrender.com/myapp/add/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formdata),
