e55e138ca7aa07261ed7a6dfcd8a819d32b6a09b
wasif-69 <wasifkhalil69@gmail.com>
2025-11-07 04:13:17 -0800

    adding APId

--- Server/collegefin/collegefin/settings.py
+++ Server/collegefin/collegefin/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # must be at the top
